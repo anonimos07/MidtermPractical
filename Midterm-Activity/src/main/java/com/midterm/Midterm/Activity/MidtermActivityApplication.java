82faa28e37c2da47a961b5c2bdb114fdfebf1c39
Anonimos0703 <williamsevenial2003@gmail.com>
2025-03-04 17:12:27 +0800

    Initial commit

---
+++ Midterm-Activity/src/main/java/com/midterm/Midterm/Activity/MidtermActivityApplication.java
@@ -0,0 +1,13 @@
+package com.midterm.Midterm.Activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermActivityApplication.class, args);
+	}
+
+}
